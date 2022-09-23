8918383c9d0fc470890a3dbf707032b17ceda61e
kareemadesola <kareemadesola1999@gmail.com>
2022-09-23 13:31:34 +0100

    updated other pages

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import Loading from "./Loading";
 import Page from "./Page";
 import Hero from "./Hero";
 
-const HOME_URL = `https://newsdata.io/api/1/news?country=ng&apikey=${process.env.REACT_APP_SECRET_KEY}`;
+const HOME_URL = `${process.env.REACT_APP_BASE_URL}?country=ng&language=en&apikey=${process.env.REACT_APP_SECRET_KEY}`;
 const Home = () => {
   const { loading, news } = useFetch(HOME_URL);
 
--- src/Sports.js
+++ src/Sports.js
@@ -1,12 +1,12 @@
-// import React from "react";
-// import { useFetch } from "./useFetch";
-// import Loading from "./Loading";
-// import Page from "./Page";
-//
-// export const SPORT_URL = `https://newsapi.org/v2/everything?q=sport&apiKey=${process.env.REACT_APP_SECRET_KEY}`;
-// const Sports = () => {
-//   const { loading, news } = useFetch(SPORT_URL);
-//   return loading ? <Loading /> : <Page news={news} />;
-// };
-//
-// export default Sports;
+import React from "react";
+import { useFetch } from "./useFetch";
+import Loading from "./Loading";
+import Page from "./Page";
+
+export const SPORT_URL = `${process.env.REACT_APP_BASE_URL}?category=sports&language=en&apiKey=${process.env.REACT_APP_SECRET_KEY}`;
+const Sports = () => {
+  const { loading, news } = useFetch(SPORT_URL);
+  return loading ? <Loading /> : <Page news={news} />;
+};
+
+export default Sports;
