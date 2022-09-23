ed04c264a4b3b43e8cce8f0a54b95bf5f64bb506
kareemadesola <kareemadesola1999@gmail.com>
2022-09-23 13:33:39 +0100

    added english to tech endpoint

--- src/Tech.js
+++ src/Tech.js
@@ -3,7 +3,7 @@ import { useFetch } from "./useFetch";
 import Loading from "./Loading";
 import Page from "./Page";
 
-const TECH_URL = `${process.env.REACT_APP_BASE_URL}?category=technology&apiKey=${process.env.REACT_APP_SECRET_KEY}`;
+const TECH_URL = `${process.env.REACT_APP_BASE_URL}?category=technology&language=en&apiKey=${process.env.REACT_APP_SECRET_KEY}`;
 const Tech = () => {
   const { loading, news } = useFetch(TECH_URL);
   return loading ? <Loading /> : <Page news={news} />;
