6a455f2e07d74cf4912c334fbea4ad7953f4b89a
kareemadesola <kareemadesola1999@gmail.com>
2022-09-30 03:56:39 +0100

    When card are clicked, redirects happens

---
+++ src/DetailedNews.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DetailedNews(props) {
+  return <div>Hello world</div>;
+}
+
+export default DetailedNews;
--- src/NewsItem.js
+++ src/NewsItem.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const NewsItem = ({ title, description, image_url }) => (
-  <Card>
-    <Card.Img
-      variant="top"
-      src={
-        image_url || "https://mdbootstrap.com/img/Photos/Others/images/76.jpg"
-      }
-    />
-    <Card.Body>
-      <Card.Title>{title}</Card.Title>
-      <Card.Text>{description}</Card.Text>
-    </Card.Body>
-  </Card>
-);
+const NewsItem = ({ title, description, image_url, link }) => {
+  function onClick() {
+    // eslint-disable-next-line no-restricted-globals
+    location.replace(link);
+  }
+  return (
+    <Card onClick={onClick} style={{ cursor: "pointer" }}>
+      <Card.Img
+        variant="top"
+        src={
+          image_url || "https://mdbootstrap.com/img/Photos/Others/images/76.jpg"
+        }
+      />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default NewsItem;
